3cd933a7133cb481758454eaaa8f29219a822670
Григорий Нагибин < >
2023-12-18 00:12:28 +0700

    Капец как я запутался!

--- anfisa_for_friends/anfisa_for_friends/urls.py
+++ anfisa_for_friends/anfisa_for_friends/urls.py
@@ -3,7 +3,8 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('homepage.urls', namespace='homepage')),
-    path('about/', include('about.urls', namespace='about')),
-    path('ice_cream/', include('ice_cream.urls', namespace='ice_cream')),
+    path('index.html', include('homepage.urls', namespace='homepage')),
+    path('about.html', include('about.urls', namespace='about')),
+    path('ice_cream_list.html', include('ice_cream.urls', namespace='ice_cream')),
     path('admin/', admin.site.urls),
 ]
