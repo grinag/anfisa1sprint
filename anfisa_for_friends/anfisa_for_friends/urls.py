e5e9a0d874140abaa369cca2aa55c291795338bc
Григорий Нагибин < >
2023-12-18 12:49:32 +0700

    Рабочая версия приложения Анфиса для Друзей

--- anfisa_for_friends/anfisa_for_friends/urls.py
+++ anfisa_for_friends/anfisa_for_friends/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('index.html', include('homepage.urls', namespace='homepage')),
     path('about.html', include('about.urls', namespace='about')),
     path('ice_cream_list.html', include('ice_cream.urls', namespace='ice_cream')),
+    path('ice_cream/', include('ice_cream.urls', namespace='ice_cream')),
     path('admin/', admin.site.urls),
 ]
